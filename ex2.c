ff920fdc8c159d2361edb0b52094d798f41eb193
kimana6378-collab <kimana6378@gmail.com>
2026-01-28 10:01:18 -0800

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+main(){
+printf("1*********\n");
+printf("12********\n");
+printf("123*******\n");
+printf("1234******\n");
+printf("12345*****\n");
+}
---
+++ ex4.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <conio.h>
+main(){
+	int a=2026-19;
+	printf("%i",a);
+}
